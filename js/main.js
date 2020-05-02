12b15ecac995ffb7b1a6dd9c36c7035d66ad7846
anna4j <anushkajadhav004@gmail.com>
2020-05-03 00:25:38 +0530

    fixing bugs

--- js/main.js
+++ js/main.js
@@ -60,3 +60,29 @@ var options = {
 
 // Adjust the CSS selector to match the container where you set up your image gallery
 var filterizr = new Filterizr('.filter-container', options);
+
+// Submit form with id function.
+function submit_by_id() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    if (validation()) // Calling validation function
+    {
+        document.getElementById("form_id").submit(); //form submission
+        alert(" Name : " + name + " n Email : " + email + " n Form Id : " + document.getElementById("form_id").getAttribute("id") + "nn Form Submitted Successfully......");
+    }
+}
+
+function validation() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+    if (name === '' || email === '') {
+        alert("Please fill all fields...!!!!!!");
+        return false;
+    } else if (!(email).match(emailReg)) {
+        alert("Invalid Email...!!!!!!");
+        return false;
+    } else {
+        return true;
+    }
+}
